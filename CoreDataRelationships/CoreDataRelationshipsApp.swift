cb5df6a003ee3055604539a0495bda79f581a478
Christian Skorobogatow <christiansko@icloud.com>
2022-07-07 08:30:38 +0200

    Initial Commit

---
+++ CoreDataRelationships/CoreDataRelationshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataRelationshipsApp.swift
+//  CoreDataRelationships
+//
+//  Created by Christian Skorobogatow on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataRelationshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
